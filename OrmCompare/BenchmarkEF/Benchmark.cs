cbbebbaa7f8913a1442b8fe723ac578035a641d0
SK <skepee01@gmail.com>
2023-09-19 10:20:45 +0100

    benchmark for EF6

---
+++ OrmCompare/BenchmarkEF/Benchmark.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkEF6
+{
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        private string _connectionstring;
+        private DbContextOptions<JsonEF_6_0.MyContext> _contextOptions;
+
+        [GlobalSetup]
+        public void Init()
+        {
+            _connectionstring = "Server=localhost;Database=DB_Orm;Trusted_Connection=True;";
+            _contextOptions = new DbContextOptionsBuilder<JsonEF_6_0.MyContext>().UseSqlServer(_connectionstring).Options;
+        }
+
+        [Benchmark]
+        public void EF6_GetSingle()
+        {
+            var context = new JsonEF_6_0.MyContext(_contextOptions);
+            _ = context.portfolio.FirstOrDefault();
+        }
+
+        [Benchmark]
+        public void EF6_GetList()
+        {
+            var context = new JsonEF_6_0.MyContext(_contextOptions);
+            _ = context.portfolio.ToList();
+        }
+    }
+}
---
+++ OrmCompare/BenchmarkEF/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using BenchmarkEF6;
+
+Console.WriteLine("Starting Benchmark EF 6");
+var res = BenchmarkRunner.Run<Benchmark>();
+Console.WriteLine("Completed Benchmark EF 6");
