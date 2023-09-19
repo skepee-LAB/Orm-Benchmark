93c661e2faef2ac6851d67690e55e12ddf0a7a9c
SK <skepee01@gmail.com>
2023-09-19 09:40:10 +0100

    Benchmark for EF 3.1

---
+++ OrmCompare/BenchmarkEF_3_1/Benchmark.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace BenchmarkEF_3_1
+{
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        private string _connectionstring;
+        private DbContextOptions<JsonEF_3_1.MyContext> _contextOptions;
+
+        [GlobalSetup]
+        public void Init()
+        {
+            _connectionstring = "Server=localhost;Database=DB_Orm;Trusted_Connection=True;";
+            _contextOptions = new DbContextOptionsBuilder<JsonEF_3_1.MyContext>().UseSqlServer(_connectionstring).Options;
+        }
+
+        [Benchmark]
+        public void EF3_1_GetSingle()
+        {
+            var context = new JsonEF_3_1.MyContext(_contextOptions);
+            _ = context.portfolio.FirstOrDefault();
+        }
+
+        [Benchmark]
+        public void EF3_1_GetList()
+        {
+            var context = new JsonEF_3_1.MyContext(_contextOptions);
+            _ = context.portfolio.ToList();
+        }
+    }
+}
---
+++ OrmCompare/BenchmarkEF_3_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace BenchmarkEF_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Benchmark EF 3.1");
+            var res = BenchmarkRunner.Run<Benchmark>();
+            Console.WriteLine("Completed Benchmark EF 3.1");
+        }
+    }
+}
