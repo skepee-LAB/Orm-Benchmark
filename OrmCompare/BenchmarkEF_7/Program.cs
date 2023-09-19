b7bdd897d475c211a8c3e54843a404164ca91b95
SK <skepee01@gmail.com>
2023-09-19 10:39:23 +0100

    Benchmark for EF7

---
+++ OrmCompare/BenchmarkEF_7/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using BenchmarkEF7;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Starting Benchmark EF 7");
+var res = BenchmarkRunner.Run<Benchmark>();
+Console.WriteLine("Completed Benchmark EF 7");
