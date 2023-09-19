b0fd83d6a4a7af54150f0c590053f640fa6a2f4d
SK <skepee01@gmail.com>
2023-09-19 10:29:47 +0100

    Benchmark for EF5

---
+++ OrmCompare/BenchmarkEF_5/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace BenchmarkEF5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Benchmark EF 5");
+            var res = BenchmarkRunner.Run<Benchmark>();
+            Console.WriteLine("Completed Benchmark EF 5");
+        }
+    }
+}
