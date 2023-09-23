438a9a49d8309de2139d7af621447797382a0a75
SK <skepee01@gmail.com>
2023-09-23 19:13:54 +0100

    added Benchmark for EF 8.

---
+++ OrmCompare/BenchmarkEF_8/Benchmark.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkEF8
+{
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        private string _connectionstring;
+        private DbContextOptions<JsonEF_8_0.MyContext> _contextOptions;
+
+        [GlobalSetup]
+        public void Init()
+        {
+            _connectionstring = "Server=localhost;Database=DB_Orm;Trusted_Connection=True;TrustServerCertificate=True";
+            _contextOptions = new DbContextOptionsBuilder<JsonEF_8_0.MyContext>().UseSqlServer(_connectionstring).Options;
+        }
+
+        [Benchmark]
+        public void EF7_GetSingle()
+        {
+            var context = new JsonEF_8_0.MyContext(_contextOptions);
+            _ = context.portfolio.FirstOrDefault();
+        }
+
+        [Benchmark]
+        public void EF7_GetList()
+        {
+            var context = new JsonEF_8_0.MyContext(_contextOptions);
+            _ = context.portfolio.ToList();
+        }
+    }
+}
---
+++ OrmCompare/BenchmarkEF_8/Program.cs
@@ -0,0 +1,6 @@
+﻿using BenchmarkEF8;
+using BenchmarkDotNet.Running;
+
+Console.WriteLine("Starting Benchmark EF 8");
+var res = BenchmarkRunner.Run<Benchmark>();
+Console.WriteLine("Completed Benchmark EF 8");
